d27fd6466df5c18d6ebe9f609e0f2317b1a3d986
Sam Walker <samwalker2312@gmail.com>
2020-01-23 11:50:23 +0000

    Added .gitignore and initial files for catalogue fitting

---
+++ fitudscatalogue.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+from loaddata import loaduds
+import bagpipes as pipes
+
+def magnitudecalc(f):
+    zpt = 3631
+    mag = -2.5*np.log(np.divide(f*(10**-3),zpt))
+    return mag
+
+filt_list = np.loadtxt('/home/s1602779/shp/filters_uds/uds_filt_list.txt', dtype='str')
+
+dblplaw = {}
+dblplaw["tau"] = (0., 15.)
+dblplaw['tau_prior'] = 'uniform'
+
+dblplaw["alpha"] = (0.1, 1000.)
+dblplaw["alpha_prior"] = "log_10"
+
+dblplaw["beta"] = (0.1, 1000.)
+dblplaw["beta_prior"] = "log_10"
+
+dblplaw["massformed"] = (1., 13.)
+dblplaw["massformed_prior"] = "log_10"
+
+dblplaw["metallicity"] = (0.1, 2.5)
+dblplaw["metallicity_prior"] = "log_10"
+
+dust = {}
+dust["type"] = "Salim"
+dust["Av"] = (0.,8.)
+dust['Av_prior'] = 'uniform'
+
+dust['delta'] = (-.3,.3)
+dust['delta_prior'] = 'Gaussian'
+dust['delta_prior_mu'] = 0.
+dust['delta_prior_sigma'] = .1
+
+dust['B'] = (0,5)
+dust['B_prior'] = 'uniform'
+
+dust["eta"] = 2.
+
+nebular = {}
+nebular["logU"] = -3.
+
+fit_instructions = {}
+fit_instructions["redshift"] = (0., 10.)
+fit_instructions["redshift_prior"] = 'uniform'
+fit_instructions["dblplaw"] = dblplaw
+fit_instructions["dust"] = dust
+fit_instructions["t_bc"] = 0.01
+fit_instructions["nebular"] = nebular
+
+########### calculate IDs for fitting here
+data = pd.read_csv('K_selected_uds_photoz_masses_photom_upload.cat', header=0, delim_whitespace=True, index_col=0)
+k_mag = data['K_iso'].apply(magnitudecalc)
+#print(k_mag)
+IDs = data.index[(data['zmed'] > 1) & (k_mag < 24)].tolist()
+print(len(IDs))
+print(IDs[:10])
+
+#perhaps truncate list using slices to first 10 or so for initial tests
+fit_cat = pipes.fit_catalogue(IDs[:10], fit_instructions, loaduds,\
+ spectrum_exists=False, cat_filt_list=filt_list, run="uds_testrun", make_plots = True)
+fit_cat.fit(verbose=False, n_live=400)
---
+++ loaddata.py
@@ -0,0 +1,151 @@
+import numpy as np
+from astropy.io import fits
+import pandas as pd
+
+##################################################
+#for photometric data
+
+def loaduds(ID):
+    dataset = pd.read_csv('K_selected_uds_photoz_masses_photom_upload.cat', header=0, delim_whitespace=True, index_col=0)
+    #IDcol = np.genfromtxt('K_selected_uds_photoz_masses_photom_upload.cat', skip_header=2, usecols=0)
+
+
+    #extracts ra and dec columns
+    #coords = dataset.loc[ID,'RA':'Dec']
+
+    #extracts values with z > 1 (presented as an example)
+    #data = dataset[dataset['zmed'] > 1.]
+    #data = data[data.index.isin(ID)]
+    ID = int(ID)
+
+    isofactor = dataset.loc[ID,'isofactor']
+
+    fluxes_nonisophot = isofactor*(dataset.loc[ID, ['U_2as', 'B_2as', 'V_2as', 'R_2as', 'i_2as', 'znew_2as', 'Y_2as', 'J_2as', 'H_2as', 'K_2as']].to_numpy())
+    fluxerrs_nonisophot = isofactor*(dataset.loc[ID, ['U_2as_err', 'B_2as_err', 'V_2as_err', 'R_2as_err', 'i_2as_err', 'znew_2as_err', 'Y_2as_err', 'J_2as_err', 'H_2as_err', 'K_2as_err']].to_numpy())
+    fluxes_irac = dataset.loc[ID, ['ch1_flux', 'ch2_flux']].to_numpy()
+    fluxerrs_irac = dataset.loc[ID, ['ch1_err', 'ch2_err']].to_numpy()
+    fluxes = np.concatenate((fluxes_nonisophot, fluxes_irac))
+    fluxerrs = np.concatenate((fluxerrs_nonisophot, fluxerrs_irac))
+    fluxes[fluxes<-98] = 0
+    fluxerrs[fluxerrs<-98] = 9.9*10**99.
+    output = np.c_[fluxes, fluxerrs]
+    print(output)
+    return output
+
+def load_goodss(ID):
+    """ Load UltraVISTA photometry from catalogue. """
+
+    # load up the relevant columns from the catalogue.
+    cat = np.loadtxt("hlsp_candels_hst_wfc3_goodss-tot-multiband_f160w_v1-1photom_cat.txt",
+                     usecols=(10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49, 52, 55,
+                              11, 14, 17, 20, 23, 26, 29, 32, 35, 38, 41, 44, 47, 50, 53, 56))
+
+    # Find the correct row for the object we want.
+    row = int(ID) - 1
+    # Extract the object we want from the catalogue.
+    fluxes = cat[row, :16]
+    fluxerrs = cat[row, 16:]
+
+    hdul = fits.open('vandels_catalogues/VANDELS_CDFS_HST_PHOT_v1.0.fits')
+    data = hdul[1].data
+    IDfield = data.field('ID')
+    IDlist = IDfield.tolist()
+    if ID in IDlist:
+        mask = (IDfield == ID)
+        fluxes[11] = data.field('Ks_HAWKI_flux')[mask][0]
+        fluxerrs[11] = data.field('Ks_HAWKI_err')[mask][0]
+    hdul.close()
+
+    # Turn these into a 2D array.
+    photometry = np.c_[fluxes, fluxerrs]
+
+    # blow up the errors associated with any missing fluxes.
+    for i in range(len(photometry)):
+        if (photometry[i, 0] == 0.) or (photometry[i, 1] <= 0):
+            photometry[i,:] = [0., 9.9*10**99.]
+
+    print(photometry)
+
+    # Enforce a maximum SNR of 20, or 10 in the IRAC channels.
+    for i in range(len(photometry)):
+        if i < 12:
+            max_snr = 20.
+
+        else:
+            max_snr = 10.
+
+        if photometry[i, 0]/photometry[i, 1] > max_snr:
+            photometry[i, 1] = photometry[i, 0]/max_snr
+
+    return photometry
+
+def load_goodss_old(ID):
+    """ Load UltraVISTA photometry from catalogue. """
+
+    # load up the relevant columns from the catalogue.
+    cat = np.loadtxt("hlsp_candels_hst_wfc3_goodss-tot-multiband_f160w_v1-1photom_cat.txt",
+                     usecols=(10, 13, 16, 19, 25, 28, 31, 34, 37, 43, 46, 49, 52, 55,
+                              11, 14, 17, 20, 26, 29, 32, 35, 38, 44, 47, 50, 53, 56))
+
+    # Find the correct row for the object we want.
+    row = int(ID) - 1
+    # Extract the object we want from the catalogue.
+    fluxes = cat[row, :14]
+    fluxerrs = cat[row, 14:]
+
+    # Turn these into a 2D array.
+    photometry = np.c_[fluxes, fluxerrs]
+
+    # blow up the errors associated with any missing fluxes.
+    for i in range(len(photometry)):
+        if (photometry[i, 0] == 0.) or (photometry[i, 1] <= 0):
+            photometry[i,:] = [0., 9.9*10**99.]
+
+    # Enforce a maximum SNR of 20, or 10 in the IRAC channels.
+    for i in range(len(photometry)):
+        if i < 10:
+            max_snr = 20.
+
+        else:
+            max_snr = 10.
+
+        if photometry[i, 0]/photometry[i, 1] > max_snr:
+            photometry[i, 1] = photometry[i, 0]/max_snr
+
+    return photometry
+
+
+####################################################################
+#for spectroscopic data
+
+def bin(spectrum, binn):
+    """ Bins up two or three column spectral data by a specified factor. """
+
+    binn = int(binn)
+    nbins = len(spectrum)/binn
+    binspec = np.zeros((nbins, spectrum.shape[1]))
+
+    for i in range(binspec.shape[0]):
+        spec_slice = spectrum[i*binn:(i+1)*binn, :]
+        binspec[i, 0] = np.mean(spec_slice[:, 0])
+        binspec[i, 1] = np.mean(spec_slice[:, 1])
+
+        if spectrum.shape[1] == 3:
+            binspec[i,2] = (1./float(binn)
+                            *np.sqrt(np.sum(spec_slice[:, 2]**2)))
+
+    return binspec
+
+
+def load_vandels_spec(ID):
+    """ Loads VANDELS spectroscopic data from file. """
+
+    hdulist = fits.open("VANDELS_CDFS_" + ID + ".fits")
+
+    spectrum = np.c_[hdulist[1].data["WAVE"][0],
+                     hdulist[1].data["FLUX"][0],
+                     hdulist[1].data["ERR"][0]]
+
+    mask = (spectrum[:,0] < 9250.) & (spectrum[:,0] > 5250.)
+
+    return bin(spectrum[mask], 2)
